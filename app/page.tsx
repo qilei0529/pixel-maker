febc2abdd9a4ad736f8636338cda6ec6b34ff157
janbluus <qilei0529@gmail.com>
2024-09-04 23:49:24 +0800

    feat: add copy

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { version } from "@/lib/utils"
 import MainView from "./views/main"
+import { Icons } from "@/shared/icons"
 
 export default function Home() {
   return (
@@ -9,6 +10,21 @@ export default function Home() {
         <span className="text-[12px] mt-1">{version}</span>
       </div>
       <MainView />
+      <div className="absolute bottom-0 w-screen left-0 z-10 h-[40px] flex flex-row items-center p-2 space-x-2 bg-white">
+        <div className="flex-1"></div>
+        <div className="flex flex-row items-center space-x-1">
+          <span className="text-[14px]">Follow</span>
+          <a
+            href="https://x.com/qilei"
+            className="flex flex-row items-center justify-center text-[12px] w-6 h-6 bg-black hover:bg-blue-600 text-white"
+            target="_blank"
+          >
+            <Icons.twitter className="w-3 h-3 text-white" />
+          </a>
+        </div>
+        <div className="text-gray-300">|</div>
+        <span className="text-[14px] ">© JAN STUDIO</span>
+      </div>
     </main>
   )
 }
