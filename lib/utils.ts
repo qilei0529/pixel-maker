a201b2180ed9c8b73c5fa9873c30e6437df0e0a8
janbluus <qilei0529@gmail.com>
2024-09-05 02:00:49 +0800

    update version

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const version = 1.1
+export const version = 1.2
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
