34ab008d08dfe722622d32f6673d690fb6ac7e4d
janbluus <qilei0529@gmail.com>
2024-10-31 22:14:11 +0800

    feat: update version

--- components/canvas/layout.tsx
+++ components/canvas/layout.tsx
@@ -17,17 +17,6 @@ export default function Layout({
   sider?: any
   rightPanel?: any
 }) {
-  const boardSize = useMemo(() => {
-    let width = Math.max(size.width, size.height) * pixelSize
-    if (width > 640) {
-      return 640
-    }
-    if (width < 400) {
-      return 400
-    }
-    return width
-  }, [size])
-
   //
   return (
     <div className="relative h-full w-full flex flex-col justify-center">
@@ -42,32 +31,6 @@ export default function Layout({
         </div>
       </div>
       <div className="absolute top-[120px] left-0 z-50">{sider}</div>
-    </div>
-  )
-  return (
-    <div className="relative overflow-auto">
-      {/* header */}
-      <div className="flex flex-row">
-        <div className="sm:w-[180px]"></div>
-        <div className="sm:w-[24px]"></div>
-        <div className="">
-          <div className="flex flex-row space-x-2">{header}</div>
-          <div className="h-4 sm:h-6"></div>
-          <div className="p-4 bg-gray-100">{}</div>
-        </div>
-        <div className="sm:w-[24px]"></div>
-        <div className="sm:w-[120px]"></div>
-      </div>
-      <div className="relative sm:absolute flex flex-col top-0 right-0 bottom-0 bg-gray-100 sm:w-[120px]">
-        <div className="sm:h-[40px]"></div>
-        <div className="flex flex-row sm:flex-col mb-4 sm:mb-0 flex-1 sm:py-4">
-          {rightPanel}
-        </div>
-      </div>
-      <div className="relative sm:absolute flex flex-col top-0 left-0 bottom-0 bg-gray-100 sm:w-[180px]">
-        <div className="sm:h-[40px]"></div>
-        <div className="flex flex-row sm:flex-col flex-1">{sider}</div>
-      </div>
     </div>
   )
 }
