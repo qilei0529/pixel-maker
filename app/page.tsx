a95df883a32cadb541ad6db808577acc4bb2d171
janbluus <qilei0529@gmail.com>
2024-10-31 21:43:10 +0800

    feat: update version

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="absolute top-0 w-screen left-0 z-10 h-[40px] flex items-center p-2 space-x-2 bg-white">
         <div className="flex-1 space-x-2 flex flex-row items-center">
           <span className="text-[18px] font-semibold">Pixel Cat</span>
-          <span className="text-[12px] mt-1">{version}</span>
+          <span className="text-[12px] mt-1">{`${version}`}</span>
           <span className="w-4"></span>
           <span className="text-[12px] text-gray-400 hidden sm:inline mt-1">
             A Pixel Painting Tool, for child, for pixel game, every thing pixel.
